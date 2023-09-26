2e47fc4bdede829004bd24a60600acb595643789
Pum-Pum-Pum-Pum <pvnkmrwrlrd@email.com>
2023-09-26 22:00:39 +0800

    setup file added

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from typing import List
+    
+# Declaring variables for setup functions
+PROJECT_NAME="housing-predictor"
+VERSION="0.1"
+AUTHOR="Pum-Pum-Pum-Pum"
+DESCRIPTION="Lets beat Phil dunphy and become the best realtor"
+PACKAGES=["housing"]
+REQUIREMENTS_FILE_NAME="requirements.txt"
+
+def get_requirements_list()->List[str]:
+    """
+    Returns:
+        A list with name of libraries from requirements.txt
+    """
+    with open(REQUIREMENTS_FILE_NAME) as requirements_file:
+        return requirements_file.readline()
+
+setup(
+    name=PROJECT_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    packages=PACKAGES,
+    install_requires=get_requirements_list()
+)
+
+
+if __name__=="__main__":
+    print(get_requirements_list())
