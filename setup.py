35fc60c3f214eb9e2d5ec62705bacb79acf18e12
Pum-Pum-Pum-Pum <pvnkmrwrlrd@email.com>
2023-10-01 20:27:52 +0800

    folders added

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from typing import List
     
 # Declaring variables for setup functions
 PROJECT_NAME="housing-predictor"
-VERSION="0.1"
+VERSION="0.0.1"
 AUTHOR="Pum-Pum-Pum-Pum"
 DESCRIPTION="Lets beat Phil dunphy and become the best realtor"
 PACKAGES=["housing"]
@@ -15,13 +15,15 @@ def get_requirements_list()->List[str]:
         A list with name of libraries from requirements.txt
     """
     with open(REQUIREMENTS_FILE_NAME) as requirements_file:
-        return requirements_file.readline()
+        # remove("-e .") is added as packages=find_packages() will take care of all the files inside housing folder
+        requirements = [line.strip() for line in requirements_file if "-e ." not in line]
+        return requirements
 
 setup(
     name=PROJECT_NAME,
     version=VERSION,
     author=AUTHOR,
     description=DESCRIPTION,
-    packages=find_packages(),#PACKAGES,
+    packages=find_packages(), #PACKAGES,
     install_requires=get_requirements_list()
 )
