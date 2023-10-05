b60b47dff0a23648f8dbe8becd8921ca235cba1e
Pum-Pum-Pum-Pum <pvnkmrwrlrd@email.com>
2023-10-05 12:40:14 +0800

    added logger

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from housing.logger import logging
 app=Flask(__name__)
-@app.route("/",methods=['GET','POST'])
 
+@app.route("/",methods=['GET','POST'])
 def index():
+    logging.info("We are testing logging module")
     return "ML project initiating..."
 
 if __name__=="__main__":
